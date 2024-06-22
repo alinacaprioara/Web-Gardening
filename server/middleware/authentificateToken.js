36d7a4ce06d9955db928f39d4d06f0054e55fb0e
anca26 <anca.iacob26@gmail.com>
2024-06-22 20:25:11 +0300

    bug_fixed

--- server/middleware/authentificateToken.js
+++ server/middleware/authentificateToken.js
@@ -3,21 +3,18 @@ const crypto = require('crypto');
 const secretKey = "tigrut"
 
 function authentificateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    console.log('Authorization Header:', authHeader);
-  
-    const token = authHeader && authHeader.split(' ')[1];
-  
-    if (token == null) return res.writeHead(401).end();
-  
-    jwt.verify(token, "tigrut", (err, user) => {
-      if (err) {
-          console.log(err);
-          return res.writeHead(403).end();
-      }
-      req.user = user;
-      next();
-    });
-  }
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.writeHead(401).end();
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+        console.log(err);
+        return res.writeHead(403).end();}
+    req.user = user;
+    next();
+  });
+}
 
 module.exports = authentificateToken;
