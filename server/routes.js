6499961014b14a8c2e0391a910a8feb0e1a04cb2
anca26 <anca.iacob26@gmail.com>
2024-06-24 18:10:43 +0300

    shopping_cart_working

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@ const cultureController = require('./controllers/cultureController');
 const authController = require('./controllers/authController');
 const flowerController = require('./controllers/flowerController');
 const userController = require('./controllers/userController');
+const shoppingCartController = require('./controllers/shoppingCartController');
 const authentificateToken = require('./middleware/authentificateToken');
 
 
@@ -17,8 +18,6 @@ const routes = {
                 res.end('Unauthorized');
                 return;
             }
-        
-            // Verify the token and get the user ID
         
             flowerController.getFlowers(req, res);
         },
@@ -62,7 +61,16 @@ const routes = {
                         res.end(JSON.stringify({ error: 'An error occurred while fetching the user\'s details' }));
                     });
             });
-},
+        },
+        '/shoppingCart': (req, res) => {
+            const token = req.headers['authorization'].split(' ')[1];
+            if (!token) {
+                res.statusCode = 401;
+                res.end('Unauthorized');
+                return;
+            }
+            shoppingCartController.getShoppingCart(req, res);
+        }
         
     },
     'POST': {
@@ -105,6 +113,16 @@ const routes = {
                 req.body = JSON.parse(body);
                 authController.changePassword(req, res);
             });
+        },
+        '/shoppingCart': (req, res) => {
+            let body = '';
+            req.on('data', chunk => {
+                body += chunk.toString();
+            });
+            req.on('end', () => {
+                req.body = JSON.parse(body);
+                shoppingCartController.addProduct(req, res);
+            });
         }
     },
 };
@@ -116,7 +134,7 @@ function handleRoute(req, res) {
 
     const routeHandler = routes[method] && routes[method][pathname];
 
-    const protectedRoutes = ['/flowers', '/cultures', '/user', '/change-password']; 
+    const protectedRoutes = ['/flowers', '/cultures', '/user', '/change-password', '/shoppingCart']; 
 
 
     if (routeHandler) {
