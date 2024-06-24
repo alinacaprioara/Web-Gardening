94c8f48144395b8d0a3bfe09b631632e36943df4
alinacaprioara <alinacaprioara13@gmail.com>
2024-06-24 17:44:25 +0300

    changes in db for cultures-planting_date

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ const userController = {
     getUserById: async function(userId) {
         try {
             const result = await db.query('SELECT * FROM users WHERE id = $1', [userId]);
-             console.log(result.rows[0]);
+            // console.log(result.rows[0]);
             return result.rows[0];
         } catch (err) {
             console.error(err);
